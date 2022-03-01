b465db23482c180f240b2fd6d29c10a8e52a0d2e
eshakkhan29 <eshakkhan29@gmail.com>
2022-03-01 10:54:28 +0600

    motamoti done

--- js/app.js
+++ js/app.js
@@ -1,11 +1,4 @@
 
-// Phone Search
-// URL Format: https://openapi.programming-hero.com/api/phones?search=${searchText}
-// Example: https://openapi.programming-hero.com/api/phones?search=iphone
-
-// Phone detail url:
-// URL Format: https://openapi.programming-hero.com/api/phone/${id}\
-// Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
 
 document.getElementById('search-button').addEventListener('click', () =>{
     document.getElementById('spinner').classList.remove('d-none')
@@ -32,21 +25,21 @@ const displayMobile = (mobiles) => {
     }
     const mobilesList = document.getElementById('mobiles-list');
     mobilesList.innerHTML = '';
-    mobiles.forEach(mobile => {
+    const mobile = mobiles.slice(0, 20);
+    mobile.forEach(mobile => {
         const div = document.createElement('div');
         div.innerHTML = `
             <img class="card-img-top" src="${mobile.image}">
             <div class="card-body text-center">
                 <h3 class="card-title text-center">${mobile.phone_name}</h3>
                 <h4 class="card-title text-center">${mobile.brand}</h4>
-                <button onclick="showDetails('${mobile.slug}')" class="btn btn-success">Show Details</button>
+                <button onclick="showDetails('${mobile.slug}')" class="btn btn-outline-success">Show Details</button>
             </div>
         `;
-        div.classList.add('col', 'card','border-0', 'mt-5','p-4')
+        div.classList.add('col', 'card','border-0', 'mt-5','p-5')
         mobilesList.appendChild(div)
         document.getElementById('error-mesege').classList.add('d-none');
-        document.getElementById('spinner').classList.add('d-none')
-
+        document.getElementById('spinner').classList.add('d-none');
     });
 };
 
@@ -59,13 +52,6 @@ const showDetails = (mobile) => {
 
 // display ditails
 const displayDitails = (mobile) => {
-    // const releaseDate = 
-    if (mobile.releaseDate === '') {
-        releaseDate = 'ReleaseDate no Found';
-    }
-    else{
-        releaseDate = mobile.releaseDate;
-    }
     const mobileDitails = document.getElementById('mobile-ditails');
     mobileDitails.innerHTML = '';
     const div = document.createElement('div');
@@ -74,7 +60,7 @@ const displayDitails = (mobile) => {
         <img src="${mobile.image}" class="card-img-top"> 
         <div class="card-body">
             <h5 class="card-title">${mobile.name}</h5>
-            <h6 class="card-title"><span class="fw-bold">ReleaseDate :</span> ${releaseDate}</h6>
+            <h6 class="card-title"><span class="fw-bold">ReleaseDate :</span> ${mobile.releaseDate ? mobile.releaseDate: 'ReleaseDate no Found'}</h6>
             <h6 class="card-title"><span class="fw-bold">Brand :</span> ${mobile.brand}</h6>
             
         </div>
@@ -92,12 +78,12 @@ const displayDitails = (mobile) => {
             <li class="list-group-item">${mobile.mainFeatures.sensors[4]}</li>
             <li class="list-group-item">${mobile.mainFeatures.sensors[5]}</li>
             <li class="list-group-item fw-bold">Others</li>
-            <li class="list-group-item">WLAN : ${mobile.others.WLAN}</li>
-            <li class="list-group-item">Bluetooth : ${mobile.others.Bluetooth}</li>
-            <li class="list-group-item">GPS : ${mobile.others.GPS}</li>
-            <li class="list-group-item">NFC : ${mobile.others.NFC}</li>
-            <li class="list-group-item">Radio : ${mobile.others.Radio}</li>
-            <li class="list-group-item">USB : ${mobile.others.USB}</li>
+            <li class="list-group-item">WLAN : ${mobile.others?.WLAN ? mobile.others.WLAN: ''}</li>
+            <li class="list-group-item">Bluetooth : ${mobile.others?.Bluetooth ? mobile.others.Bluetooth: ''}</li>
+            <li class="list-group-item">GPS : ${mobile.others?.GPS ? mobile.others.GPS: ''}</li>
+            <li class="list-group-item">NFC : ${mobile.others?.NFC ? mobile.others.NFC: ''}</li>
+            <li class="list-group-item">Radio : ${mobile.others?.Radio ? mobile.others.Radio: ''}</li>
+            <li class="list-group-item">USB : ${mobile.others?.USB ? mobile.others.USB: ''}</li>
         </ul>
     </div>
     `;
